1903110423527eb767045f6ebcc4a872f38ed6b3
Bradley Shawyer <21098019+bradleyess@users.noreply.github.com>
2020-10-08 17:14:03 +1100

    improvement: less opinionated index file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import * as P5 from "p5"
 
 new P5((p5: P5) => {
-  p5.setup = () => {
-    p5.createCanvas(window.innerWidth, window.innerHeight)
-    p5.background(0)
-  }
-
+  p5.preload = () => {}
+  p5.setup = () => {}
   p5.draw = () => {}
 })
